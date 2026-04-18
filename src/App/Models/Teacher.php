7866efb3e645eb03e359870a21af454c36531fe5
aziz65 <zbakh.aziz65@hotmail.com>
2026-04-18 11:39:40 +0100

    Create Teacher model class that extends User.

---
+++ src/App/Models/Teacher.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User; // Assuming User is in the same namespace
+
+class Teacher extends User
+{
+    // Teacher-related properties and methods go here
+}
