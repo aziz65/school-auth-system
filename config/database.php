bdf620c883338ae25df867bcd8077f35662d26f6
aziz65 <zbakh.aziz65@hotmail.com>
2026-04-18 11:37:28 +0100

    Create config/database.php with database connection configuration

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database configuration
+return [
+    'host' => 'localhost',
+    'database' => 'your_database_name',
+    'username' => 'your_database_username',
+    'password' => 'your_database_password',
+];
